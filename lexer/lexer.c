61d8d26c31bbacd686df5ac3177f42458713736c
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2023-02-12 16:22:41 +0530

    lexer changes Co-authored-by: Ruchika Sarkar <Ruchika1684@users.noreply.github.com>

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -3,27 +3,20 @@
 // #include<stdbool.h>
 #include"lexer.h"
 
-// int tokenizer(TwinBuffer TB,LEXEME lex){
-//     char* currBuff = TB.curr?TB.buffFirst:TB.buffSecond;
-//     char input[20];
-//     int i = 0;
-//     while(TB.lexemeBegin < TB.forward){
-//         if(i == 20){
-//             return 0;
-//         }
-//         input[i] = TB
-//     }
-// }
 
-void simulateDFA(){
+void tokenizer(TwinBuffer TB,LEXEME lex,int type){
+    // char* input = incrementLexemeBegin(TB);
+}
+
+
+void simulateDFA(TwinBuffer TB){
     char* error;
     int state = 0;
     char c;
-    TwinBuffer TB;
     int lineCount = 0;
     char* currBuff;
     while(1){
-        currBuff = TB.currentBuffer?TB.buffFirst:TB.buffSecond;
+        // currBuff = TB.currentBuffer?TB.buffFirst:TB.buffSecond;
         switch(state){
             case -1:
                 error = "Lexical Error Occured";
@@ -31,7 +24,7 @@ void simulateDFA(){
             case 0:
                 printf("START STATE\n");
                 // TB.lexemeBegin = TB.forward;
-                c = currBuff[TB.forward];
+                c = getCharacterAtForward(TB);
                 if(c == '+')       state = 1;
                 else if (c == '-') state = 2;
                 else if (c == '/') state = 3;
@@ -59,101 +52,101 @@ void simulateDFA(){
             
             case 1:
                 printf("STATE 1\n");
-                ++TB.forward;
+                incrementForward(TB);
                 printf("TOKENIZE TK_PLUS\n");
                 state = 0;
                 break;
             case 2:
                 printf("STATE 2\n");
-                ++TB.forward;
+                incrementForward(TB);
                 printf("TOKENIZE TK_MINUS\n");
                 state = 0;
                 break;
             case 3:
                 printf("STATE 3\n");
-                ++TB.forward;
+                incrementForward(TB);
                 printf("TOKENIZE TK_DIV\n");
                 state = 0;
                 break;
             case 4:
                 printf("STATE 4\n");
-                ++TB.forward;
+                incrementForward(TB);
                 printf("TOKENIZE TK_SEMICOL\n");
                 state = 0;
                 break;
             case 5:
                 printf("STATE 5\n");
-                ++TB.forward;
+                incrementForward(TB);
                 printf("TOKENIZE TK_COMMA\n");
                 state = 0;
                 break;
             case 6:
                 printf("STATE 6\n");
-                ++TB.forward;
+                incrementForward(TB);
                 printf("TOKENIZE TK_SQBO\n");
                 state = 0;
                 break;
             case 7:
                 printf("STATE 7\n");
-                ++TB.forward;
+                incrementForward(TB);
                 printf("TOKENIZE TK_SQBC\n");
                 state = 0;
                 break;
             case 8:
                 printf("STATE 8\n");
-                ++TB.forward;
+                incrementForward(TB);
                 printf("TOKENIZE TK_BO\n");
                 state = 0;
                 break;
             case 9:
                 printf("STATE 9\n");
-                ++TB.forward;
+                incrementForward(TB);
                 printf("TOKENIZE TK_BC\n");
                 state = 0;
                 break;
             case 10:
                 printf("STATE 10\n");
-                ++TB.forward;
-                c = currBuff[TB.forward];
+                incrementForward(TB);
+                c = getCharacterAtForward(TB);
                 if(c == '=') state = 11;
                 else state = -1;
                 break;
             case 11:
                 printf("STATE 11\n");
-                ++TB.forward;
+                incrementForward(TB);
                 printf("TOKENIZE TK_NE\n");
                 state = 0;
                 break;
             case 12:
                 printf("STATE 12\n");
-                ++TB.forward;
-                c = currBuff[TB.forward];
+                incrementForward(TB);
+                c = getCharacterAtForward(TB);
                 if(c == '=') state = 13;
                 else state = -1;
                 break;
             case 13:
                 printf("STATE 13\n");
-                ++TB.forward;
+                incrementForward(TB);
                 printf("TOKENIZE TK_EQ\n");
                 state = 0;
                 break;
             case 14:
                 printf("STATE 14\n");
-                ++TB.forward;
-                c = currBuff[TB.forward];
+                incrementForward(TB);
+                c = getCharacterAtForward(TB);
                 if(c == '.') state = 15;
                 else state = -1;
                 break;
             case 15:
                 printf("STATE 15\n");
-                ++TB.forward;
+                incrementForward(TB);
                 printf("TOKENIZE TK_RANGEOP\n");
                 state = 0;
                 break;
             case 16:
                 printf("STATE 16\n");
-                ++TB.forward;
-                c = currBuff[TB.forward];
+                incrementForward(TB);
+                c = getCharacterAtForward(TB);
                 if(c == '*') state = 17;
                 else{
                     printf("TOKENIZE TK_MUL\n");
@@ -162,28 +155,28 @@ void simulateDFA(){
                 break;
             case 17:
                 printf("STATE 17\n");
-                ++TB.forward;
-                c = currBuff[TB.forward];
+                incrementForward(TB);
+                c = getCharacterAtForward(TB);
                 if(c == '*') state = 18;
                 else state = 17;
                 break;
             case 18:
                 printf("STATE 18\n");
-                ++TB.forward;
-                c = currBuff[TB.forward];
+                incrementForward(TB);
+                c = getCharacterAtForward(TB);
                 if(c == '*') state = 19;
                 else state = 17;
                 break;
             case 19:
                 printf("STATE 19\n");
-                ++TB.forward;
+                incrementForward(TB);
                 printf("TOKENIZE TK_COMMENTMARK\n");
                 state = 0;
                 break;
             case 20:
                 printf("STATE 20\n");
-                ++TB.forward;
-                c = currBuff[TB.forward];
+                incrementForward(TB);
+                c = getCharacterAtForward(TB);
                 if(c == '=') state = 21;
                 else{
                     printf("TOKENIZE TK_COLON\n");
@@ -192,10 +185,29 @@ void simulateDFA(){
                 break;
             case 21:
                 printf("STATE 21\n");
-                ++TB.forward;
+                incrementForward(TB);
                 printf("TOKENIZE TK_ASSIGNOP\n");
                 state = 0;
                 break;
+            case 22:
+                printf("STATE 22\n");
+                incrementForward(TB);
+                c = getCharacterAtForward(TB);
+                if(c == '.') state = 23;
+                else if(c >= '0' && c <= '9') state = 22;
+                else{
+                    printf("TOKENIZE TK_NUM\n");
+                    state = 0;
+                    break;
+                }
+            case 23:
+                printf("STATE 23\n");
+                incrementForward(TB);
+                c = getCharacterAtForward(TB);
+                if(c >= '0' && c <= '9') state = 24;
+                else if(c == '.'){
+                    //decreament forward
+                }
         }
     }
 }
