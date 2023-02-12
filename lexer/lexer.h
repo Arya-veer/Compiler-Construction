5c1fd5c3062650975872cb94333ff4b44119968b
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2023-02-12 15:50:15 +0530

    changes

--- lexer/lexer.h
+++ lexer/lexer.h
@@ -2,7 +2,7 @@
 #define LEXER_LEXER_H
 
 #include"../utils/tokens/tokens.h"
-#include"../utils/readFile/readFile.h"
+#include"../utils/Buffer/readFile.h"
 
 typedef struct lexeme LEXEME;
 struct lexeme{
