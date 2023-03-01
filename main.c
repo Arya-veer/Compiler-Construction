6392924685cbfad0552a875436daf2ad916088e2
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2023-03-01 14:58:59 +0530

    made switch case for options

--- main.c
+++ main.c
@@ -2,11 +2,35 @@
 #include "argparse/index.h"
 
 int main(int argc, char* argv[]){
-    Arguments* args = argparse(argc, argv);
-    // printf("%s", args[FILENAME]);
-    char* grammarFile = "manual/grammar.csv";
-    char* inputFile = "codeInput.txt";
-    parser(grammarFile,inputFile);
-    // testAutomation(grammarFile);
-    return 0;
+    int option = -1;
+    
+    while(option){
+        switch (option)
+        {
+            case 0:
+                return 0;
+                break;
+
+            case 1:
+                printf("1\n");
+                break;
+
+            case 2:
+                printf("2\n");
+                break;
+
+            case 3:
+                printf("3\n");
+                break;
+
+            case 4:
+                printf("4\n");
+                break;
+            
+            default:
+                break;
+        }
+        printf("Enter your option: ");
+        scanf(" %d", &option);
+    }
 }
